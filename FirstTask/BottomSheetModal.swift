628918d7e57329a5297a550225ef2d211870638d
mallowlabs <mallowlabs@gmail.com>
2020-08-10 12:44:41 +0900

    Auto format

--- FirstTask/BottomSheetModal.swift
+++ FirstTask/BottomSheetModal.swift
@@ -1,38 +1,38 @@
 import SwiftUI
 
 struct BottomSheetModal<Content: View>: View {
-  @Binding var isShown: Bool
-  @Binding var height: CGFloat
-
-  var content: () -> Content
-
-  var body: some View {
-    ZStack(alignment: .bottom) {
-      if isShown {
-        background
-        modal
-      }
+    @Binding var isShown: Bool
+    @Binding var height: CGFloat
+    
+    var content: () -> Content
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            if isShown {
+                background
+                modal
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
-    .edgesIgnoringSafeArea(.all)
-  }
-
-  private var background: some View {
-    Color.black
-          .frame(
-            minWidth: 0,
-            maxWidth: .infinity,
-            minHeight: 0,
-            maxHeight: .infinity,
-            alignment: .center)
-          .opacity(0.65)
-          .animation(.spring())
-          .gesture(TapGesture().onEnded { self.isShown = false })
-  }
-
-  private var modal: some View {
-    self.content()
-      .frame(width: UIScreen.main.bounds.width, height: height, alignment: .top)
-      .background(Color(UIColor.systemBackground))
-      .cornerRadius(10)
-  }
+    
+    private var background: some View {
+        Color.black
+            .frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .center)
+            .opacity(0.65)
+            .animation(.spring())
+            .gesture(TapGesture().onEnded { self.isShown = false })
+    }
+    
+    private var modal: some View {
+        self.content()
+            .frame(width: UIScreen.main.bounds.width, height: height, alignment: .top)
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(10)
+    }
 }
