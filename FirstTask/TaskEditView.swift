004fccf5625a96f28a17d17801dfe2bad5c1d74f
mallowlabs <mallowlabs@gmail.com>
2020-08-13 21:07:35 +0900

    Added: edit modal

--- FirstTask/TaskEditView.swift
+++ FirstTask/TaskEditView.swift
@@ -1,21 +1,34 @@
-//
-//  TaskEditView.swift
-//  FirstTask
-//
-//  Created by Toshinori OSUKA on 2020/08/13.
-//  Copyright © 2020 codefirst. All rights reserved.
-//
-
 import SwiftUI
 
 struct TaskEditView: View {
+    @ObservedObject var task: Task
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Input title", text: $task.wrappedTitle)
+                Section(header: Text("Memo")) {
+                    TextField("", text: $task.wrappedMemo)
+                }
+            }.navigationBarTitle("Edit Task")
+                .navigationBarItems(trailing: Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Image(systemName: "xmark")
+                        .frame(width: 40, height: 40)
+                        .imageScale(.large)
+                        .foregroundColor(Color(UIColor(named: "Accent")!))
+                        .clipShape(Circle())
+                })
+        }
     }
 }
 
 struct TaskEditView_Previews: PreviewProvider {
     static var previews: some View {
-        TaskEditView()
+        let task = Task.make(id: UUID(), title: "ミルクを買う")
+        task.memo = "住まいは田舎がいい、森と日溜まりでひと寝入り、飛ぶ鳥、稲と日照り、まだ独りもいいが、家内はいます"
+        return TaskEditView(task: task)
     }
 }
