579151e875801d7122f098304d78946b74ccb26c
mallowlabs <mallowlabs@gmail.com>
2020-08-27 20:42:21 +0900

    Refactored: use BottomTextFieldSheetModal

--- FirstTask/TaskList.swift
+++ FirstTask/TaskList.swift
@@ -64,31 +64,9 @@ struct TaskList: View {
                     }.padding(10)
                 }.padding(10)
 
-                BottomSheetModal(isShown: $appSettings.showAddTaskModal) {
-                    GeometryReader { geometry in
-                        HStack {
-                            FocusableTextField(text: self.$newTaskTitle, isFirstResponder: true) { _ in }
-                                .frame(width: geometry.size.width - 40, height: 50)
-                                .keyboardType(.default)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                            Button(action: {
-                                _ = Task.create(context: self.viewContext, title: self.$newTaskTitle.wrappedValue)
-                                self.$newTaskTitle.wrappedValue = ""
-                                self.appSettings.showAddTaskModal = false
-                                UIApplication.shared.closeKeyboard()
-                            }) {
-                                Image(systemName: "arrow.up")
-                                    .frame(width: 40, height: 40)
-                                    .imageScale(.large)
-                                    .background(Color(UIColor(named: "Accent")!))
-                                    .foregroundColor(.white)
-                                    .clipShape(Circle())
-                            }
-                        }
-                    }
-                    .padding()
-                    .frame(height: 80)
+                BottomTextFieldSheetModal(isShown: $appSettings.showAddTaskModal, text: self.$newTaskTitle) {
+                    _ = Task.create(context: self.viewContext, title: self.$newTaskTitle.wrappedValue)
+                    self.appSettings.showAddTaskModal = false
                 }
             }
         }
