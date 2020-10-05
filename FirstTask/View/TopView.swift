babbb2aeefde688ccdfe4afa7cb48f2ac9239208
mallowlabs <mallowlabs@gmail.com>
2020-10-05 10:50:03 +0900

    Added: FAB on TopView

--- FirstTask/View/TopView.swift
+++ FirstTask/View/TopView.swift
@@ -3,25 +3,45 @@ import SwiftUI
 struct TopView: View {
     @Environment(\.managedObjectContext) var viewContext
 
+    @State var showingAddTaskModal = false
+    @State var newTaskTitle: String = ""
+
     var body: some View {
         NavigationView {
-            List {
-                ProjectRow(icon: "tray", name: "Inbox") { _ in
-                    true // XXX has no projects
-                }
-                ProjectRow(icon: "star", name: "Today") { task in
-                    task.hasTagByKind(tagKind: "today")
-                }
+            ZStack(alignment: .bottom) {
+                List {
+                    ProjectRow(icon: "tray", name: "Inbox") { _ in
+                        true // XXX has no projects
+                    }
+                    ProjectRow(icon: "star", name: "Today") { task in
+                        task.hasTagByKind(tagKind: "today")
+                    }
 
-                Section(header: Text("Projects")) {
-                    HStack {
-                        Image(systemName: "plus")
-                        Text("Add Project")
+                    Section(header: Text("Projects")) {
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("Add Project")
+                        }
                     }
                 }
+                .listStyle(GroupedListStyle())
+                .navigationBarTitle("FirstTask")
+
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        FabButton {
+                            self.showingAddTaskModal = true
+                        }
+                    }.padding(10)
+                }.padding(10)
+
+                BottomTextFieldSheetModal(isShown: self.$showingAddTaskModal, text: self.$newTaskTitle) {
+                    _ = Task.create(context: self.viewContext, title: self.$newTaskTitle.wrappedValue)
+                    self.showingAddTaskModal = false
+                }
             }
-            .listStyle(GroupedListStyle())
-            .navigationBarTitle("FirstTask")
         }
     }
 }
