3bf6c9618c78c719a8724f641feef813845e0206
mallowlabs <mallowlabs@gmail.com>
2020-10-05 15:03:34 +0900

    Changed: move setting button to TopView

--- FirstTask/ObservableObject/ModalState.swift
+++ FirstTask/ObservableObject/ModalState.swift
@@ -2,6 +2,5 @@ import SwiftUI
 
 class ModalState: ObservableObject {
     @Published var showingEditModal = false
-    @Published var showingSettingMenuModal = false
     @Published var showingSearchModal = false
 }
