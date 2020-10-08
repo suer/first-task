88d0ad19ff2b64fbfb8d1b5db9720227a0b72074
mallowlabs <mallowlabs@gmail.com>
2020-10-08 15:57:14 +0900

    Added: ProjectEditView

---
+++ FirstTask/View/ProjectEditView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct ProjectEditView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @Environment(\.presentationMode) var presentationMode
+    @State var project: Project = Project()
+
+    var body: some View {
+        NavigationView {
+            ProjectFormView(project: project)
+            .navigationBarTitle("Edit Project")
+            .navigationBarItems(leading: cancelButton, trailing: saveButton)
+        }
+    }
+
+    private var saveButton: some View {
+        Button(action: {
+            self.project.save(context: self.viewContext)
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("Save")
+        }
+    }
+
+    private var cancelButton: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("Cancel")
+        }
+    }
+}
+
+struct ProjectEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectEditView()
+            .environment(\.managedObjectContext, CoreDataSupport.context)
+    }
+}
