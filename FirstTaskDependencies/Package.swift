7d4a72ea40aec1e3bddda21e46249964a8c42c7c
mallowlabs <mallowlabs@gmail.com>
2025-10-14 09:13:29 +0900

    Implement Package.swift

--- FirstTaskDependencies/Package.swift
+++ FirstTaskDependencies/Package.swift
@@ -5,22 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "FirstTaskDependencies",
+    platforms: [
+        .iOS(.v18)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FirstTaskDependencies",
             targets: ["FirstTaskDependencies"]
-        ),
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "12.3.0"),
+        .package(url: "https://github.com/firebase/FirebaseUI-iOS.git", exact: "15.1.0"),
+        .package(url: "https://github.com/mono0926/LicensePlist.git", exact: "3.27.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FirstTaskDependencies"
-        ),
-        .testTarget(
-            name: "FirstTaskDependenciesTests",
-            dependencies: ["FirstTaskDependencies"]
-        ),
+            name: "FirstTaskDependencies",
+            dependencies: [
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuthUI", package: "FirebaseUI-iOS"),
+                .product(name: "FirebaseGoogleAuthUI", package: "FirebaseUI-iOS")
+            ]
+        )
     ]
 )
